7d56ee4e40fc70dc74b9948b4e8672143e77e8af
xiaobaishu1 <au-ychen@foxmail.com>
2026-04-03 14:45:58 +0800

    Update http_server_api.rs

--- plugin/smartdns-ui/src/http_server_api.rs
+++ plugin/smartdns-ui/src/http_server_api.rs
@@ -759,53 +759,64 @@ impl API {
         Ok(response)
     }
 
-    async fn get_devices_from_luci(data_server: &Arc<DataServer>) -> Result<Vec<DeviceInfo>, HttpError> {
-        let output = tokio::task::spawn_blocking(|| {
-            Command::new("ubus")
-                .args(&["call", "luci-rpc", "getHostHints"])
-                .output()
-        }).await
-          .map_err(|e| HttpError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
-          .map_err(|e| HttpError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+async fn get_devices_from_luci(data_server: &Arc<DataServer>) -> Result<Vec<DeviceInfo>, HttpError> {
+    let output_result = tokio::task::spawn_blocking(|| {
+        Command::new("ubus")
+            .args(&["call", "luci-rpc", "getHostHints"])
+            .output()
+    }).await;
 
-        let json: Value = serde_json::from_slice(&output.stdout)
-            .map_err(|e| HttpError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-
-        let mut devices = Vec::new();
-        if let Value::Object(map) = json {
-            for (mac, info) in map {
-                let ipaddrs = info.get("ipaddrs").and_then(|v| v.as_array())
-                    .map(|arr| arr.iter().filter_map(|v| v.as_str().map(String::from)).collect())
-                    .unwrap_or_default();
-                let ip6addrs = info.get("ip6addrs").and_then(|v| v.as_array())
-                    .map(|arr| arr.iter()
-                        .filter_map(|v| v.as_str().map(String::from))
-                        .filter(|ip| !ip.starts_with("fe80:"))
-                        .collect())
-                    .unwrap_or_default();
-                let hostname = info.get("name").and_then(|v| v.as_str()).unwrap_or("").to_string();
-
-                let last_query_ts = data_server.get_last_query_timestamp_by_mac(&mac)
-                    .map_err(|e| HttpError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
-                    .unwrap_or(0);
-
-                devices.push(DeviceInfo {
-                    id: 0,
-                    mac: mac.clone(),
-                    hostname,
-                    ipv4_list: ipaddrs,
-                    ipv6_list: ip6addrs,
-                    last_query_timestamp: last_query_ts,
-                });
-            }
+    let output = match output_result {
+        Ok(Ok(output)) => output,
+        _ => {
+            dns_log!(LogLevel::WARN, "Failed to execute ubus command, returning empty device list");
+            return Ok(Vec::new());
         }
+    };
 
-        devices.sort_by(|a, b| b.last_query_timestamp.cmp(&a.last_query_timestamp));
-        for (i, dev) in devices.iter_mut().enumerate() {
-            dev.id = (i + 1) as u64;
+    let json: Value = match serde_json::from_slice(&output.stdout) {
+        Ok(v) => v,
+        Err(e) => {
+            dns_log!(LogLevel::WARN, "Failed to parse ubus output: {}, returning empty", e);
+            return Ok(Vec::new());
         }
-        Ok(devices)
+    };
+
+    let mut devices = Vec::new();
+    if let Value::Object(map) = json {
+        for (mac, info) in map {
+            let ipaddrs = info.get("ipaddrs").and_then(|v| v.as_array())
+                .map(|arr| arr.iter().filter_map(|v| v.as_str().map(String::from)).collect())
+                .unwrap_or_default();
+            let ip6addrs = info.get("ip6addrs").and_then(|v| v.as_array())
+                .map(|arr| arr.iter()
+                    .filter_map(|v| v.as_str().map(String::from))
+                    .filter(|ip| !ip.starts_with("fe80:"))
+                    .collect())
+                .unwrap_or_default();
+            let hostname = info.get("name").and_then(|v| v.as_str()).unwrap_or("").to_string();
+
+            let last_query_ts = data_server.get_last_query_timestamp_by_mac(&mac)
+                .map_err(|e| HttpError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
+                .unwrap_or(0);
+
+            devices.push(DeviceInfo {
+                id: 0,
+                mac: mac.clone(),
+                hostname,
+                ipv4_list: ipaddrs,
+                ipv6_list: ip6addrs,
+                last_query_timestamp: last_query_ts,
+            });
+        }
     }
+
+    devices.sort_by(|a, b| b.last_query_timestamp.cmp(&a.last_query_timestamp));
+    for (i, dev) in devices.iter_mut().enumerate() {
+        dev.id = (i + 1) as u64;
+    }
+    Ok(devices)
+}
 
     async fn api_log_stream(
         this: Arc<HttpServer>,
